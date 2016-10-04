2c4fbc5675faea0467b6afe69103a3044d3185c9
Jessica Chiang <jessicachiang@Jessica-Chiangs-MacBook-2.local>
2016-10-03 20:32:33 -0700

    debugger worked

--- writeJsHere.js
+++ writeJsHere.js
@@ -1,1 +1,17 @@
 //All your code goes here
+var myBank = function (password){
+var myAccount = {
+balance : 5,
+password: 1234
+};
+if (myAccount.password === password){
+	return myAccount.balance;
+} else {
+	return 'Try Again';
+}
+
+};
+
+console.log (myBank (1234));
+
+console.log (myBank (1264));
